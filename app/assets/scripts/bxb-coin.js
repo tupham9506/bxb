f2ee75b85686398239279f729f9e3f14db6bcd65
tom-backend <tom.pham@vinova.com.sg>
2023-10-04 12:35:01 +0700

    Note

--- app/assets/scripts/bxb-coin.js
+++ app/assets/scripts/bxb-coin.js
@@ -2,28 +2,42 @@ function BxbCoin() {
   this.mempool = []
   this.chain = []
   this.difficulty = 5
-  this.bonus = 100
+  this.reward = 100
+
+  /**
+   * Tạo khối genesis
+   */
   this.init = () => {
     const block = {
       time: Date.parse('2000-01-01'),
       data: [],
-      nonce: 0
+      nonce: 0,
+      prevHash: ''
     }
 
     block.hash = this.createHash(block)
     this.chain.push(block)
   }
 
+  /**
+   * Tạo hash cho các dữ liệu trong khối
+   */
   this.createHash = block => {
     return window.CryptoJS.SHA256(block.prevHash + block.time + JSON.stringify(block.data) + block.nonce).toString()
   }
 
+  /**
+   * Đặt chữ ký cho transaction
+   */
   this.sign = transaction => {
     return window.walletSecret
       .sign(window.CryptoJS.SHA256(transaction.from, transaction.to, transaction.amount).toString(), 'base64')
       .toDER('hex')
   }
 
+  /**
+   * Tạo 1 block
+   */
   this.createBlock = data => {
     for (let transaction of data) {
       if (transaction.from) {
@@ -38,13 +52,16 @@ function BxbCoin() {
     }
   }
 
+  /**
+   * Mint
+   */
   this.mint = (block, address) => {
     block.hash = ''
     block.nonce = 0
     block.data.push({
       from: '',
       to: address,
-      amount: this.bonus
+      amount: this.reward
     })
 
     while (block.hash.substring(0, this.difficulty) !== Array(this.difficulty + 1).join('0')) {
@@ -55,6 +72,9 @@ function BxbCoin() {
     return block
   }
 
+  /**
+   * Thêm vào blockchain
+   */
   this.addToChain = block => {
     let prevBlock = this.chain[this.chain.length - 1]
     if (prevBlock.hash === block.prevHash) {
