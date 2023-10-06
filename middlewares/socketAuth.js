a62e2554a2328af8bb7fa383e93b6c5fb6afdd1c
tom-backend <tom.pham@vinova.com.sg>
2023-10-06 17:48:14 +0700

    rm auth

--- middlewares/socketAuth.js
+++ middlewares/socketAuth.js
@@ -2,21 +2,22 @@ const User = require('./../models/user')
 const jwt = require('jsonwebtoken')
 
 module.exports = async (socket, next) => {
-  const token = socket.handshake.auth.token
-
-  try {
-    var auth = jwt.verify(token, process.env.JWT_SECRET)
-  } catch (err) {
-    return next(new Error('Unauthorized!'))
-  }
-
-  const user = await User.findOne({
-    _id: auth.id
-  })
-
-  if (!user) return next(new Error('Unauthorized!'))
-
-  socket.auth = user
+  // const token = socket.handshake.auth.token
+  //
+  // try {
+  //   var auth = jwt.verify(token, process.env.JWT_SECRET)
+  // } catch (err) {
+  //   return next(new Error('Unauthorized!'))
+  // }
+  //
+  // const user = await User.findOne({
+  //   _id: auth.id
+  // })
+  //
+  // if (!user) return next(new Error('Unauthorized!'))
+  //
+  // socket.auth = user
+  socket.auth = {}
 
   next()
 }
